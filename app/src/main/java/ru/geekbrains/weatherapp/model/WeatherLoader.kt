696c590bdab8519ae26eedd76b5bd84f06248791
HomchenkoP <homchenkop@yandex.ru>
2021-06-04 19:10:19 +0300

    Повторено практическое задание методички Урока 5.

---
+++ app/src/main/java/ru/geekbrains/weatherapp/model/WeatherLoader.kt
@@ -0,0 +1,60 @@
+package ru.geekbrains.weatherapp.model
+
+import ru.geekbrains.weatherapp.BuildConfig
+
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import com.google.gson.Gson
+import java.net.MalformedURLException
+import java.net.URL
+import javax.net.ssl.HttpsURLConnection
+
+class WeatherLoader(
+    private val listener: WeatherLoaderListener,
+    private val lat: Double,
+    private val lon: Double
+) {
+
+    fun loadWeather() {
+        try {
+            val uri =
+                URL("https://api.weather.yandex.ru/v2/forecast?lat=${lat}&lon=${lon}&lang=ru_RU")
+            val handler = Handler(Looper.getMainLooper())
+            Thread(Runnable {
+                lateinit var urlConnection: HttpsURLConnection
+                try {
+                    urlConnection = uri.openConnection() as HttpsURLConnection
+                    urlConnection.requestMethod = "GET"
+                    urlConnection.addRequestProperty(
+                        "X-Yandex-API-Key",
+                        BuildConfig.WEATHER_API_KEY
+                    )
+                    urlConnection.readTimeout = 3_000
+
+                    val weatherDTO: WeatherDTO = Gson().fromJson(
+                        urlConnection.inputStream.bufferedReader(),
+                        WeatherDTO::class.java
+                    )
+
+                    handler.post { listener.onLoaded(weatherDTO) }
+                } catch (e: Exception) {
+                    Log.e("", "Fail connection", e)
+                    e.printStackTrace()
+                    listener.onFailed(e)
+                } finally {
+                    urlConnection.disconnect()
+                }
+            }).start()
+        } catch (e: MalformedURLException) {
+            Log.e("", "Fail URI", e)
+            e.printStackTrace()
+            listener.onFailed(e)
+        }
+    }
+
+    interface WeatherLoaderListener {
+        fun onLoaded(weatherDTO: WeatherDTO)
+        fun onFailed(throwable: Throwable)
+    }
+}
--- app/src/main/java/ru/geekbrains/weatherapp/view/DetailsFragment.kt
+++ app/src/main/java/ru/geekbrains/weatherapp/view/DetailsFragment.kt
@@ -4,11 +4,15 @@ import ru.geekbrains.weatherapp.R
 import ru.geekbrains.weatherapp.argumentNullable
 import ru.geekbrains.weatherapp.databinding.FragmentDetailsBinding
 import ru.geekbrains.weatherapp.model.Weather
+import ru.geekbrains.weatherapp.model.WeatherDTO
+import ru.geekbrains.weatherapp.model.WeatherLoader
 
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 
 class DetailsFragment : Fragment() {
@@ -19,7 +23,7 @@ class DetailsFragment : Fragment() {
         fun newInstance(weather: Weather): DetailsFragment =
             DetailsFragment().apply {
                 this.weatherData = weather
-                }
+            }
     }
 
     private var _binding: FragmentDetailsBinding? = null
@@ -27,28 +31,51 @@ class DetailsFragment : Fragment() {
         // геттер переменной binding
         get(): FragmentDetailsBinding = _binding!!
 
+    private val onLoadListener: WeatherLoader.WeatherLoaderListener =
+        object : WeatherLoader.WeatherLoaderListener {
+
+            override fun onLoaded(weatherDTO: WeatherDTO) {
+                displayWeather(weatherDTO)
+            }
+
+            override fun onFailed(throwable: Throwable) {
+                //Обработка ошибки
+            }
+        }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentDetailsBinding.inflate(inflater, container, false)
-        return binding.getRoot()
+        return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.mainView.visibility = View.GONE
+        binding.loadingLayout.visibility = View.VISIBLE
         weatherData?.let { weatherData ->
-            weatherData.city.also { city ->
-                binding.cityName.text = city.name
-                binding.cityCoordinates.text = String.format(
-                    getString(R.string.city_coordinates),
-                    city.lat,
-                    city.lon
-                )
-            }
-            binding.temperatureValue.text = weatherData.temperature.toString()
-            binding.feelsLikeValue.text = weatherData.feelsLike.toString()
+            WeatherLoader(onLoadListener, weatherData.city.lat, weatherData.city.lon).loadWeather()
+        }
+    }
+
+    private fun displayWeather(weatherDTO: WeatherDTO) {
+        with(binding) {
+            mainView.visibility = View.VISIBLE
+            loadingLayout.visibility = View.GONE
+            val city = weatherData?.city
+            cityName.text = city?.name
+            cityCoordinates.text = String.format(
+                getString(R.string.city_coordinates),
+                city?.lat.toString(),
+                city?.lon.toString()
+            )
+            weatherCondition.text = weatherDTO.fact?.condition
+            temperatureValue.text = weatherDTO.fact?.temp.toString()
+            feelsLikeValue.text = weatherDTO.fact?.feelsLike.toString()
         }
     }
 
