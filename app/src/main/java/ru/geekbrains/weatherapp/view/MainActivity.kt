c83f3332d1a4ff07f9fcdf22a23542f68a5d5a1e
HomchenkoP <homchenkop@yandex.ru>
2021-06-08 18:01:42 +0300

    1. Подпишитесь на событие изменения связи (CONNECTIVITY_ACTION) и уведомляйте об этом пользователя.

--- app/src/main/java/ru/geekbrains/weatherapp/view/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/weatherapp/view/MainActivity.kt
@@ -1,19 +1,30 @@
 package ru.geekbrains.weatherapp.view
 
+import ru.geekbrains.weatherapp.MainBroadcastReceiver
 import ru.geekbrains.weatherapp.R
 
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val receiver = MainBroadcastReceiver()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                    .replace(R.id.container, MainFragment.newInstance())
-                    .commitNow()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
         }
+        registerReceiver(receiver, IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
+    }
+
+    override fun onDestroy() {
+        unregisterReceiver(receiver)
+        super.onDestroy()
     }
 }
