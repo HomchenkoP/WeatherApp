50799a570cffa3da06d349e0dce182a3f248ca90
HomchenkoP <homchenkop@yandex.ru>
2021-06-09 14:08:01 +0300

    Регистрация MainBroadcastReceiver привязана к жизненному циклу MainActivity.

--- app/src/main/java/ru/geekbrains/weatherapp/MainBroadcastReceiver.kt
+++ app/src/main/java/ru/geekbrains/weatherapp/MainBroadcastReceiver.kt
@@ -3,17 +3,39 @@ package ru.geekbrains.weatherapp
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.net.ConnectivityManager
 import android.widget.Toast
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
 
-class MainBroadcastReceiver : BroadcastReceiver() {
+class MainBroadcastReceiver(private val context: Context?) : BroadcastReceiver(),
+    LifecycleObserver {
 
     override fun onReceive(context: Context, intent: Intent) {
-        StringBuilder().apply {
-            append("СООБЩЕНИЕ ОТ СИСТЕМЫ\n")
-            append("Action: ${intent.action}")
-            toString().also {
-                Toast.makeText(context, it, Toast.LENGTH_LONG).show()
+        if (intent.action == ConnectivityManager.CONNECTIVITY_ACTION) {
+            StringBuilder().apply {
+                append("СООБЩЕНИЕ ОТ СИСТЕМЫ\n")
+                append("Action: ${intent.action}")
+                toString().also {
+                    Toast.makeText(context, it, Toast.LENGTH_SHORT).show()
+                }
             }
+        }
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+    fun registerReceiver() {
+        context?.let {
+            it.registerReceiver(this, IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
+        }
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+    fun unregisterReceiver() {
+        context?.let {
+            it.unregisterReceiver(this)
         }
     }
 }
--- app/src/main/java/ru/geekbrains/weatherapp/view/MainActivity.kt
+++ app/src/main/java/ru/geekbrains/weatherapp/view/MainActivity.kt
@@ -3,14 +3,10 @@ package ru.geekbrains.weatherapp.view
 import ru.geekbrains.weatherapp.MainBroadcastReceiver
 import ru.geekbrains.weatherapp.R
 
-import android.content.IntentFilter
-import android.net.ConnectivityManager
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-
-    private val receiver = MainBroadcastReceiver()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,11 +16,8 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
-        registerReceiver(receiver, IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION))
-    }
-
-    override fun onDestroy() {
-        unregisterReceiver(receiver)
-        super.onDestroy()
+        MainBroadcastReceiver(this).let {
+            lifecycle.addObserver(it)
+        }
     }
 }
