8de700a256412bf278519e91613aadbf24f073ed
HomchenkoP <homchenkop@yandex.ru>
2021-06-11 19:40:23 +0300

    Добавлен Interceptor логирования http-запроса / ответа сервера.

--- app/src/main/java/ru/geekbrains/weatherapp/repository/RemoteDataSource.kt
+++ app/src/main/java/ru/geekbrains/weatherapp/repository/RemoteDataSource.kt
@@ -1,12 +1,15 @@
 package ru.geekbrains.weatherapp.repository
 
-import ru.geekbrains.weatherapp.BuildConfig
-import ru.geekbrains.weatherapp.model.WeatherDTO
-
 import com.google.gson.GsonBuilder
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Callback
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import ru.geekbrains.weatherapp.BuildConfig
+import ru.geekbrains.weatherapp.model.WeatherDTO
+import java.io.IOException
 
 class RemoteDataSource {
 
@@ -17,9 +20,25 @@ class RemoteDataSource {
                 GsonBuilder().setLenient().create()
             )
         )
+        .client(createOkHttpClient(WeatherApiInterceptor()))
         .build().create(YandexWeatherAPI::class.java)
 
     fun getWeatherDetails(lat: Double, lon: Double, callback: Callback<WeatherDTO>) {
         weatherApi.getWeather(BuildConfig.WEATHER_API_KEY, lat, lon, "ru_RU").enqueue(callback)
+    }
+
+    private fun createOkHttpClient(interceptor: Interceptor): OkHttpClient {
+        val httpClient = OkHttpClient.Builder()
+        httpClient.addInterceptor(interceptor)
+        httpClient.addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+        return httpClient.build()
+    }
+
+    inner class WeatherApiInterceptor : Interceptor {
+
+        @Throws(IOException::class)
+        override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
+            return chain.proceed(chain.request())
+        }
     }
 }
